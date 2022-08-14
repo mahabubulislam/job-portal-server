1019dfa5da20afeb6bf78e365d21be382cfe221d
mahbub-webdev <mahabub2k1@gmail.com>
2022-08-14 11:07:12 +0600

    user personal info update api added

--- index.js
+++ index.js
@@ -12,27 +12,37 @@ app.use(cors())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9kz3i.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
+async function run() {
     try {
-       await client.connect()
-       const userCollection = client.db('jobHaunt').collection('users')
+        await client.connect()
+        const userCollection = client.db('jobHaunt').collection('users')
 
-       app.post('/users', async (req,res)=>{
-        const user = req.body;
-        const result = await userCollection.insertOne(user)
-        res.send(result)
-       })
-        
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+
+        app.patch('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = {email:email}
+            const updateDoc = {
+                $set: { address: req.body.address, phoneNumber:req.body.phoneNumber }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
+
     }
-    finally{
+    finally {
 
     }
 }
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Hello From Server')
 })
 
 run().catch(console.dir);
-app.listen(port, ()=>console.log('listening from', port))
+app.listen(port, () => console.log('listening from', port))
